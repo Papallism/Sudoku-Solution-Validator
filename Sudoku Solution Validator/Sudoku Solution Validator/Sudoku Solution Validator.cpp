96c92c228339f9192784c4dfed546b7b199f1e5b
Michalis-PC <mihalispapallis@gmail.com>
2021-05-04 19:58:12 +0300

    Add sudoku solution constant

--- Sudoku Solution Validator/Sudoku Solution Validator/Sudoku Solution Validator.cpp
+++ Sudoku Solution Validator/Sudoku Solution Validator/Sudoku Solution Validator.cpp
@@ -9,6 +9,17 @@
 
 #include <iostream>
 
+// constant 2D array for the Sudoku solution to be checked
+const int sudoku[9][9] = {  {6, 2, 4, 5, 3, 9, 1, 8, 7},
+							{5, 1, 9, 7, 2, 8, 6, 3, 4},
+							{8, 3, 7, 6, 1, 4, 2, 9, 5},
+							{1, 4, 3, 8, 6, 5, 7, 2, 9},
+							{9, 5, 8, 2, 4, 7, 3, 6, 1},
+							{7, 6, 2, 3, 9, 1, 4, 5, 8},
+							{3, 7, 1, 9, 5, 6, 8, 4, 2},
+							{4, 9, 6, 1, 8, 2, 5, 7, 3},
+							{2, 8, 5, 4, 7, 3, 9, 1, 6} };
+
 /**
 *  Function <code>main</code> is the main entry point of the application
 *  <BR>
